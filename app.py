7cfded74debf8942f65c5c7993437c0de2ffbaeb
Balan2206 <sakthishamifb@gmail.com>
2023-08-09 00:07:29 +0530

    updated final code ,just need to add the search option in cart

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class User(db.Model,UserMixin):
     password = db.Column(db.String(150), nullable=False)
 
     # Override get_id method to return the user_id as a string for the expected id value
-    def get_id(self):   
+    def get_id(self):
         return str(self.user_id)
 
 
@@ -30,7 +30,8 @@ class Cart(db.Model,UserMixin):
     cart_id=db.Column(db.Integer, primary_key=True)
     product_name=db.Column(db.String, db.ForeignKey("product.product_name"))   
     category_id=db.Column(db.Integer, db.ForeignKey("categories.category_id"))
-
+    user_id=db.Column(db.Integer, db.ForeignKey("user.user_id"))
+    quantity=db.Column(db.Integer, nullable=False, default=1)
     # Override get_id method to return the cart_id as a string for the expected id value
     def get_id(self):   
         return str(self.cart_id)
@@ -49,7 +50,7 @@ class Admin(db.Model,UserMixin):
 class Categories(db.Model,UserMixin):
     category_id=db.Column(db.Integer, primary_key=True)
     type=db.Column(db.String, nullable=False)
-    products=db.relationship("Product",backref="categories")
+    #products=db.relationship("Product",backref="categories")
 
     # Override get_id method to return the category_id as a string for the expected id value
     def get_id(self):   
@@ -111,15 +112,6 @@ def userlogin():
                     session.pop('remember_me', None)
 
                 return redirect(url_for('home'))
-            
-                '''
-                if remember:
-                    login_user(user, remember=True) #this remember attribute will remember that the user is logged in until they clear the browsing history or logout manually or refreshes the server
-                    return redirect(url_for('home'))
-                else:
-                    login_user(user, remember=False)
-                    return redirect(url_for('home'))
-                '''
             else:
                 flash('Incorrect password, try again', category='error')
         else:
@@ -130,14 +122,16 @@ def userlogin():
 @app.route('/admin',methods=['POST','GET'])
 @login_required
 def adminselect():
+    '''
     if request.method=='POST':
-        x=request.form.get('select')
+        x=request.form.get('value')
         if x=='add':
             return redirect('/adminadd')
         elif x=='delete':
             return redirect('/admindelete')
         else:
             flash('Enter a valid command (either add or delete)')
+     '''       
     return render_template('adminselect.html',user=current_user)
 
 @app.route('/adminadd', methods=['POST','GET'])
@@ -149,7 +143,6 @@ def addcategory():
         price=request.form.get('price')
         quantity=request.form.get('quantity')
         unit=request.form.get('unit')
-
 
         c = Categories(
             type = category
@@ -166,7 +159,7 @@ def addcategory():
         db.session.add(c)
         db.session.add(p)
         db.session.commit()
-
+        flash('Added the category to database!')
     else:
         flash('Enter the names before clicking add!')
     return render_template('adminadd.html',user=current_user)
@@ -206,12 +199,16 @@ def home():
         selected_category_id = request.form.get('category_id')
         selected_product = request.form.get('product_name')
         selected_category = Categories.query.get(selected_category_id)
-
+        selected_quantity=int(request.form.get('quantity'))
         #adding the product to cart
         if selected_category:
             #new_cart_category = Categories.query.get(selected_category_id)
             #new_cart_product = Product.query.get(selected_product)
-            new_cart_entry = Cart(product_name=selected_product,category_id=selected_category_id)
+            new_cart_entry = Cart(
+                product_name=selected_product,
+                category_id=selected_category_id,
+                user_id=current_user.user_id,
+                quantity=selected_quantity)
             db.session.add(new_cart_entry)
             db.session.commit()
             flash('Product successfully added to the cart!', category='Success')
@@ -222,6 +219,34 @@ def home():
     all_data=zip(all_c,all_p)
     return render_template("home.html", all_data=all_data, user = current_user, selected_category=selected_category)
 
+@app.route('/cart', methods=['POST','GET'])
+@login_required
+def cart():
+    user_cart_entries=Cart.query.filter_by(user_id=current_user.user_id).all()
+    #all_cr=Cart.query.all()
+    return render_template("cart.html",user_cart_entries=user_cart_entries,user=current_user)
+
+@app.route('/cartdelete', methods=['POST','GET'])
+@login_required
+def cartdelete():
+    if request.method=='POST':
+        cart_id=request.form.get('cart_id')
+        cart_entry = Cart.query.get(cart_id)  # Retrieve the cart entry using the ID
+        if cart_entry:
+            db.session.delete(cart_entry)  # Delete the cart entry from the database
+            db.session.commit()
+            flash('Item removed from cart successfully!', category='Success')
+        else:
+            flash('Cart entry not found!', category='error')
+
+    return redirect('/cart')  # Redirect back to the cart page
+'''
+        c = Cart.query.get(cart_id).all()
+
+        db.session.delete(c)
+        db.session.commit()
+    return redirect('/cart')
+'''
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
@@ -255,13 +280,6 @@ def signup():
 
     
     return render_template('signup.html', user = current_user)
-
-@app.route('/cart', methods=['POST','GET'])
-@login_required
-def cart():
-    all_cr=Cart.query.all()
-    return render_template("cart.html",all_cr=all_cr)
-
 
 @app.route('/logout', methods=['GET','POST'])
 @login_required
